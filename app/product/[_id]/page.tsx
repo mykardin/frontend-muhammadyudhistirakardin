b76815b0c1fab98a56d8471adc9e9c6288edb363
mykardin <111116439+mykardin@users.noreply.github.com>
2025-01-23 14:46:57 +0700

    Update Detail Product

--- app/product/[_id]/page.tsx
+++ app/product/[_id]/page.tsx
@@ -90,15 +90,6 @@ export default function DetailProduct({params} : any) {
           <div className="mt-4 lg:row-span-3 lg:mt-0">
             <h2 className="sr-only">Product information</h2>
             <p className="text-3xl tracking-tight text-gray-900">Price : Rp. {price}</p>
-
-            <form className="mt-10">
-              <button
-                type="submit"
-                className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                Update Product
-              </button>
-            </form>
             <form onClick={deleteProductByID} className="mt-10">
               <button
                 type="submit"
